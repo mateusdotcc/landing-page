4458c6a0175656d773642d7e5d18bf438d883757
Mateus Henrique <msnmateus@live.com>
2023-07-03 19:52:51 -0300

    feat: main gallery

---
+++ src/app/components/Gallery.tsx
@@ -0,0 +1,17 @@
+export function Gallery() {
+  return (
+    <section className={'flex w-full items-center justify-center'}>
+      <div className={'w-full max-w-[1140px] pb-[92px] pt-[92px]'}>
+        <nav>
+          <ul>
+            <li>Menu</li>
+          </ul>
+        </nav>
+
+        <ul>
+          <li>card component</li>
+        </ul>
+      </div>
+    </section>
+  )
+}
